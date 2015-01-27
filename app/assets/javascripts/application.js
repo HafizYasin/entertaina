54af1010a2beb6e258bbfca0c5a32e0a5a50f83a
lakesare <eugekrns@yandex.ru>
2015-01-28 00:36:40 +0500

    otto

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,6 +29,48 @@ var eventName = (isIPod() || isIPad() || isIPhone()) ? "touchstart" : "click";
 window.onload=function(){
 $(function(){
 
+
+	$('.text_editor .toolbar a').click(function(e) {
+		switch($(this).data('role')) {
+			case 'h1':
+			case 'h2':
+				var isH = document.queryCommandValue("h2");
+				if (isH){
+					document.execCommand('formatBlock', false, 'div');
+				} else{
+					document.execCommand('formatBlock', false, $(this).data('role'));
+				};
+
+				break;
+			default:
+				document.execCommand($(this).data('role'), false, null);
+				break;
+		}
+	});
+
+	
+
+
+
+	$('#currency .ui-selectmenu-button').append('<i class="fa fa-chevron-down"></i>');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	//_________________________modals
 	$('.fancy').fancybox({ fitToView: false, autoResize: false, autoSize: true, padding: 0 });
 	$('.fancy_img').fancybox({ fitToView: false, autoResize: false, autoSize: true, padding: 30 });
@@ -65,3 +107,13 @@ $(function(){
 
 });
 }
+
+
+
+
+
+
+
+
+
+
