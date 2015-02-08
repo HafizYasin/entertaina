45607ff8e85f45973f16e3e1e6f0713a0b75890d
lakesare <eugekrns@yandex.ru>
2015-02-08 22:22:50 +0200

    god I forgot one more thing, still fixing heroku deploy

--- app/emails/a.rb
+++ app/emails/a.rb
@@ -1,18 +1,18 @@
-# require 'premailer'
+# # require 'premailer'
 
-premailer = Premailer.new('http://localhost:3000/emails/welcome', :warn_level => Premailer::Warnings::SAFE)
+# premailer = Premailer.new('http://localhost:3000/emails/welcome', :warn_level => Premailer::Warnings::SAFE)
 
-# Write the HTML output
-File.open("output.html", "w") do |fout|
-  fout.puts premailer.to_inline_css
-end
+# # Write the HTML output
+# File.open("output.html", "w") do |fout|
+#   fout.puts premailer.to_inline_css
+# end
 
-# Write the plain-text output
-File.open("output.txt", "w") do |fout|
-  fout.puts premailer.to_plain_text
-end
+# # Write the plain-text output
+# File.open("output.txt", "w") do |fout|
+#   fout.puts premailer.to_plain_text
+# end
 
-# Output any CSS warnings
-premailer.warnings.each do |w|
-  puts "#{w[:message]} (#{w[:level]}) may not render properly in #{w[:clients]}"
-end
+# # Output any CSS warnings
+# premailer.warnings.each do |w|
+#   puts "#{w[:message]} (#{w[:level]}) may not render properly in #{w[:clients]}"
+# end
