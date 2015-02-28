976ca9477bb6089256ce379d7003da5f62ccbf51
lakesare <eugekrns@yandex.ru>
2015-02-28 05:27:47 +0300

    _

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -209,12 +209,13 @@ $('input.date').bind("keypress", function(e){
 })
 
 //move to search coffee
-$('header video').get(0).load();
-$('header video').get(0).play();
-$('header video').on('ended', function () {
-	$('header video').get(0).load();
-	$('header video').get(0).play();
-});
+// var video = $('header video').get(0);
+// video.load();
+// video.play();
+// $('header video').on('ended', function () {
+// 	video.load();
+// 	video.play();
+// });
 
 
 
